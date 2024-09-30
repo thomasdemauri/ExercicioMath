efadd4bdffb1d0cb9d15a00769b5c1a5a9853eea
Thomas Mauro <thom_mauro@outlook.com>
2024-09-30 20:54:44 -0300

    rotas principais adicionadas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 'view';
-});
+Route::get('/', [MainController::class, 'home'])->name('home');
+Route::get('/print-exercises', [MainController::class, 'print'])->name('print');
+Route::get('/export', [MainController::class, 'export'])->name('export');
+Route::post('/generate', [MainController::class, 'generate'])->name('generate');
