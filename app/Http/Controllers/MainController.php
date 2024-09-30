b6af5f20afe611917a21ed6673a1f6b7b732c30c
Thomas Mauro <thom_mauro@outlook.com>
2024-09-30 20:55:34 -0300

    criacao dos principais metodos

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function home()
+    {
+
+    }
+
+    /**
+     * Generate exercises
+     */
+    public function generate(Request $request)
+    {
+
+    }
+
+    /**
+     * Print exercises in browser
+     */
+    public function print()
+    {
+
+    }
+
+    /**
+     * Export exercises to a .txt file
+     */
+    public function export()
+    {
+
+    }
+}
