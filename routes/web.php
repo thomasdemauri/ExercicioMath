166bf955e639741528e77e6b13fba655469079fd
Thomas Mauro <thom_mauro@outlook.com>
2024-09-30 23:14:17 -0300

    rota de visualizacao dos exercicios adiconada

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'home'])->name('home');
+Route::get('/view-exercises', [MainController::class, 'viewExercises'])->name('home');
 Route::get('/print-exercises', [MainController::class, 'print'])->name('print');
 Route::get('/export', [MainController::class, 'export'])->name('export');
 Route::post('/generate', [MainController::class, 'generate'])->name('generate');
